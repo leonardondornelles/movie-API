4d3ef671942423eca57fe0a1c3b9037549ef0b3c
Leonardo Noronha Dornelles <leonardondornelles@gmail.com>
2025-08-28 19:11:39 -0300

    reviews WIP

--- database.js
+++ database.js
@@ -13,19 +13,67 @@ let movies = [
     }
 ];
 
-let reviews = [
-    {
-        id: 1,
-        movieId: 1,
-        text: "Fiquei maluco vendo sério",
-        rating: 5
-    },
-    {
-        id: 2,
-        movieId: 2,
-        text: "Doideira a simulação e tals",
-        rating: 5
-    }
+let reviews =[
+  {
+    "id": 1,
+    "movieId": 1,
+    "text": "Fiquei maluco vendo sério",
+    "rating": 5
+  },
+  {
+    "id": 2,
+    "movieId": 2,
+    "text": "Doideira a simulação e tals",
+    "rating": 5
+  },
+  {
+    "id": 3,
+    "movieId": 1,
+    "text": "O roteiro é um pouco confuso, mas os efeitos especiais são incríveis.",
+    "rating": 4
+  },
+  {
+    "id": 4,
+    "movieId": 3,
+    "text": "Uma obra-prima do cinema! Atuações impecáveis e uma história emocionante.",
+    "rating": 5
+  },
+  {
+    "id": 5,
+    "movieId": 2,
+    "text": "Não gostei muito do final, achei que poderia ter sido melhor desenvolvido.",
+    "rating": 3
+  },
+  {
+    "id": 6,
+    "movieId": 4,
+    "text": "Filme perfeito para ver com a família, muito divertido e leve.",
+    "rating": 4
+  },
+  {
+    "id": 7,
+    "movieId": 3,
+    "text": "Esperava mais, a história não me prendeu.",
+    "rating": 2
+  },
+  {
+    "id": 8,
+    "movieId": 5,
+    "text": "Que fotografia espetacular! Cada cena é uma pintura.",
+    "rating": 5
+  },
+  {
+    "id": 9,
+    "movieId": 1,
+    "text": "Achei bem mediano, não entendi todo o hype em cima desse filme.",
+    "rating": 3
+  },
+  {
+    "id": 10,
+    "movieId": 5,
+    "text": "A trilha sonora é o ponto alto do filme, simplesmente fantástica.",
+    "rating": 4
+  }
 ];
 
 module.exports = {
--- index.js
+++ index.js
@@ -11,9 +11,23 @@ app.use(express.json());
 // Define a porta em que o servidor vai rodar
 const port = 3000;
 
+
+
 // GETS ----------------------
 
+// Rota principal - Apenas uma mensagem de boas-vindas
+app.get('/', (req, res) => {
+  console.log("http://localhost:3000/")
+  res.send('Bem-vindo à API de Filmes e Reviews!');
+});
+
 // MOVIES ID --------------------------
+
+// Nova rota GET para '/movies' em geral
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
 app.get('/movies/:id', (req, res) => {
   const id = req.params.id;
   const filmeEncontrado = movies.find(filme => filme.id === parseInt(id));
@@ -33,33 +47,58 @@ app.get('/movies/:id/reviews', (req, res) => {
   if(!filmeEncontrado){
     res.status(404).send("Filme não encontrado");
   }else{
-    if(reviewEncontrada.length < 0){
-      res.status(404).send("Review não encontrado");
+    if(reviewEncontrada.length === 0){
+      res.status(200).send("Review não encontrado");
     }else{
       res.status(200).json(reviewEncontrada);
     }
   }
 });
 
-// Rota principal - Apenas uma mensagem de boas-vindas
-app.get('/', (req, res) => {
-  console.log("http://localhost:3000/")
-  res.send('Bem-vindo à API de Filmes e Reviews!');
-});
+app.get('/movies/:id/reviews/:reviewId', (req, res) => {
+  const movieId = req.params.id;
+  const reviewId = req.params.reviewId;
 
-// Nova rota GET para '/movies' em geral
-app.get('/movies', (req, res) => {
-  res.json(movies);
-});
+  const filmeEncontrado = movies.find(filme => filme.id === parseInt(movieId));
+
+  if(!filmeEncontrado){
+    return res.status(404).send("Filme não encontrado");
+  }
+
+  const reviewEncontrada = reviews.find(review => review.id === parseInt(reviewId));
+
+  if(!reviewEncontrada){
+    return res.status(404).send("Review não encontrada");
+  }
+
+  if(filmeEncontrado.id !== reviewEncontrada.movieId)
+  {
+    res.status(400).send("Essa review não pertence a este filme");
+  }else{
+    res.status(200).json(reviewEncontrada);
+  }
+})
 
 // POSTS --------------
-
 app.post('/movies', (req, res) => {
     const newMovie = req.body;
     
     movies.push(newMovie);
-    // fuasfjsaifisaugiigu
+
     res.status(201).json(newMovie);
+})
+
+app.post('/movies:id/reviews', (req, res) => {
+    const id = req.params.id;
+    const newReview = req.body;
+    const filmeEncontrado = movies.find(filme => filme.id === parseInt(id));
+
+    if(!filmeEncontrado){
+      res.status(404).send("Filme não encontrado");
+    }else{
+        reviews.push(newReview);
+    }
+    res.status(201).json(newReview);
 })
 
 // PUT ------------------
@@ -77,6 +116,34 @@ app.put('/movies/:id', (req, res) =>{
 
       res.status(200).json(filmeEncontrado);
     }
+});
+
+app.put('/movies/:id/reviews/:reviewId', (req, res) => {
+  const id = req.params.id;
+  const reviewId = req.params.reviewId;
+  const newReview = req.body;
+
+  const filmeEncontrado = movies.find(filme => filme.id === parseInt(id));
+
+    if(!filmeEncontrado){
+      return res.status(404).send("Filme não encontrado");
+    }
+  
+    const reviewEncontrada = reviews.find(review => review.id);
+
+  if(!reviewEncontrada){
+    return res.status(404).send("Review não encontrada");
+  }
+
+  if(filmeEncontrado.id !== reviewEncontrada.movieId)
+  {
+    res.status(400).send("Essa review não pertence a este filme");
+  }else{
+    reviewEncontrada.text = req.body.text;
+    reviewEncontrada.rating = req.body.rating;
+
+    res.status(200).json(reviewEncontrada);
+  }
 });
 
 // DELETE --------------------
